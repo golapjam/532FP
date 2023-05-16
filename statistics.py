a199621f66723e566a6fd9701dec51a6cf1fa1f2
James Mooney <jmmooney@umass.edu>
2023-05-16 12:49:47 -0400

    Changes to be committed: 	modified:   statistics.py 	modified:   textAnalysis.py

--- statistics.py
+++ statistics.py
@@ -4,6 +4,14 @@ import matplotlib.pyplot as plt
 
 
 def getStats(dataframe):
+    """
+    Driver method for calculating statistics
+
+    dataframe: pyspark.sql.DataFrame
+        The corpus of an author, each row is a line of text
+
+    Returns 
+    """
     wordCounts = wordCount(dataframe)
     smallWords = getSmallWords(wordCounts)
     midlengthWords = getMidWords(wordCounts)
@@ -14,26 +22,71 @@ def getStats(dataframe):
     return wordCounts, smallWords, midlengthWords, largeWords
 
 def wordCount(dataframe):
+    """
+    Calculate word count through DF functions
+    
+    dataframe: pyspark.sql.DataFrame
+        The corpus of an author, each row is a line of text
+    
+    Returns DataFrame of word counts
+    """
     wordCounts = dataframe.groupBy("words").count().orderBy(functions.desc("count")).alias("counts").cache()
     wordCounts.show()
     return wordCounts
 
 def getSmallWords(dataframe):
+    """
+    Filter small words through DF functions
+    (small defined as 1 <= len <= 7)
+    
+    dataframe: pyspark.sql.DataFrame
+        The corpus of an author, each row is a line of text
+    
+    Returns DataFrame of small words
+    """
     smallWords = dataframe.filter(functions.length("words") <= 7).cache()
     smallWords.show()
     return smallWords
 
 def getMidWords(dataframe):
+    """
+    Filter mid-length words through DF functions
+    (mid defined as 7 < len <= 14)
+    
+    dataframe: pyspark.sql.DataFrame
+        The corpus of an author, each row is a line of text
+    
+    Returns DataFrame of mid-length words
+    """
     midlengthWords = dataframe.filter(functions.length("words") > 7).filter(functions.length("words") <= 14).cache()
     midlengthWords.show()
     return midlengthWords
 
 def getLargeWords(dataframe):
+    """
+    Filter large words through DF functions
+    (large defined as 14 < len)
+    
+    dataframe: pyspark.sql.DataFrame
+        The corpus of an author, each row is a line of text
+    
+    Returns DataFrame of large words
+    """
     largeWords = dataframe.filter(functions.length("words") > 14)
     largeWords.show()
     return largeWords
 
 def plot_word_distribution_chart(smallWords, midlengthWords, largeWords):
+    """
+    Plot chart of word distributions based on counts of small, mid, and large words
+
+    smallWords: pyspark.sql.DataFrame
+        DataFrame containing only small words
+    midlengthWords: pyspark.sql.DataFrame
+        DataFrame containing only mid-length words
+    largeWords: pyspark.sql.DataFrame
+        DataFrame containing only large words
+    """
 
     # combine aggregate of word counts by to their length
     smallWords_count = smallWords.agg({"count": "sum"})
--- textAnalysis.py
+++ textAnalysis.py
@@ -7,47 +7,67 @@ from statistics import getStats
 # -*- coding: utf-8 -*-
 
 def mainLoop():
-    authors = input('Enter Authors\' Names Separated by Comma (First Last, First Last): ').split()
+    authors = input('Enter 2 Authors\' Names Separated by Comma (First Last, First Last): ').split()
     while len(authors) != 4:
         print('Please use format First Last, First Last exactly (drop middle names).')
-        authors = input('Enter Authors\' Names Separated by Comma (First Last, First Last): ').split()
+        authors = input('Enter 2 Authors\' Names Separated by Comma (First Last, First Last): ').split()
     corpus = getBooks(authors)
-    while len(corpus) != 2:
+    while len(corpus) != 2: 
         print('Please choose another pair of authors for comparison')
         print('(Hint: most books available are those in public domain,\n so try searching for authors published before 1923)')
         authors = input('Enter Authors\' Names Separated by Comma (First Last, First Last): ').split()
         corpus = getBooks(authors)
+    # now that we have our corpus we can begin SparkSession to clean and calculate
     spark = SparkSession.builder.appName('TextAnalysis').getOrCreate()
+    # right now corpus is list of two big lines of text ['book...', 'book...']
+    # we want each row of our DF to be a line of text within the book, so we must write to .txt file
+    # in order to convert \n to newline
     outF1 = open('corpus1.txt', 'w', encoding='utf-8-sig')
     outF1.write(corpus[0])
     outF1.close()
     outF2 = open('corpus2.txt', 'w', encoding='utf-8-sig')
     outF2.write(corpus[1])
     outF2.close()
+    # read in our data to Spark DF
     logData1 = spark.read.text('corpus1.txt').cache()
     logData2 = spark.read.text('corpus2.txt').cache()
-    numAs = logData1.filter(logData1.value.contains('a')).count()
-    print('Lines with a: %i\n' % numAs)
+    # clean data
     logData1 = cleanData(logData1)
     logData2 = cleanData(logData2)
+    # calculate stats and show visualizations
     print(f'Stats for {authors[0]}:')
     statData1 = getStats(logData1)
     print(f'Stats for {authors[1]}:')
     statData2 = getStats(logData2)
     spark.stop()
-    if os.path.exists('corpus.txt'):
-        os.remove('corpus.txt')
+    # remove temporary corpus files
+    if os.path.exists('corpus1.txt'):
+        os.remove('corpus1.txt')
+    if os.path.exists('corpus2.txt'):
+        os.remove('corpus2.txt')
 
 
 def getBooks(authors):
-    res = []
+    """
+    Download corpus for authors through gutendex API requests
+
+    -----------------
+    authors: list
+        The two authors whose work we want to fetch
+
+    -----------------
+    Return list of text
+    """
+    res = [] # for storing found text
     a1 = authors[:2]
     a1[1] = a1[1][:-1]
     a2 = authors[2:]
+    # build query for gutendex request
     query1 = 'https://gutendex.com/books?search=complete%20works%20' + a1[0].lower() + '%20' + a1[1].lower()
     query2 = 'https://gutendex.com/books?search=complete%20works%20' + a2[0].lower() + '%20' + a2[1].lower()
-    print(query1)
-    print(query2)
+    # request loop for (query, author) pairs
+    # first try to find 'Complete Works of X' which would give largest dataset
+    # if no results, search for any books by author and pull first result
     for q, a in zip([query1, query2], [a1, a2]):
         author = '' + a[0] + ' ' + a[1]
         try:
@@ -57,11 +77,13 @@ def getBooks(authors):
             SystemError(e)
         except requests.exceptions.RequestException as e:
             SystemExit(e)
+        # convert to JSON for accessibility
         asJSON = req.json()
         req.close()
         if asJSON['count'] == 0:
             print(f'Could not find complete works for \'{author}\', trying individual works...')
             query = 'https://gutendex.com/books/?search=' + a[0] + '%20' + a[1]
+            # now try for any books
             try:
                 req = requests.get(query)
                 req.raise_for_status()
@@ -73,7 +95,7 @@ def getBooks(authors):
             req.close()
             if asJSON['count'] == 0:
                 print(f'Could not find works for \'{author}\'')
-                return res
+                return res # return to main function to try different author
             else:
                 txt = asJSON['results'][0]['formats']['text/plain']
             try:
